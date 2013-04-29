c215ae65b4a82bf722f3b10cb7b8e1e314b949c8
Sergey Vasilenko <svasilenko@mirantis.com>
2013-04-29 18:30:02 +0400

    fix for puppet2.7

--- lib/puppet/parser/functions/cidr_to_netmask.rb
+++ lib/puppet/parser/functions/cidr_to_netmask.rb
@@ -3,7 +3,15 @@
 # cidr_to_netmask.rb
 #
 require 'ipaddr'
-require 'puppet/parser/functions/lib/prepare_cidr.rb'
+begin
+  require 'puppet/parser/functions/lib/prepare_cidr.rb'
+rescue LoadError => e
+  # puppet apply does not add module lib directories to the $LOAD_PATH (See
+  # #4248). It should (in the future) but for the time being we need to be
+  # defensive which is what this rescue block is doing.
+  rb_file = File.join(File.dirname(__FILE__),'lib','prepare_cidr.rb')
+  load rb_file if File.exists?(rb_file) or raise e
+end
 
 module Puppet::Parser::Functions
   newfunction(:cidr_to_netmask, :type => :rvalue, :doc => <<-EOS
--- manifests/l2/centos_upndown_scripts.pp
+++ manifests/l2/centos_upndown_scripts.pp
@@ -1,3 +1,4 @@
+# see http://tech.serbinn.net/2012/custom-script-on-interface-up-down-centos-and-rhel/
 class l23network::l2::centos_upndown_scripts {
   file {'/sbin/ifup-local':
     ensure  => present,
