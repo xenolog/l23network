6a61b9aca289021273cdee9cfd17af8bdcd273d9
Sergey Vasilenko <svasilenko@mirantis.com>
2015-08-06 11:58:47 +0300

    Add ip address duplication check
    * Add ip address duplication check for l3_ifconfig puppet resource
    before ip address set. Sometimes when there is ip address duplication
    it leads to wierd problems and long-time investigation of root cause.
    * Add sending Gratuitous ARP to update all neighbours.
    
    Author: Stanislav Makar <smakar@mirantis.com>
    
    Closes: #62

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,6 +16,7 @@ class l23network::params {
       $ovs_datapath_package_name = undef
       $ovs_common_package_name   = 'openvswitch-switch'
       $ovs_kern_module_name      = 'openvswitch'
+      $extra_tools               = 'iputils-arping'
     }
     /(?i)redhat/: {
       $interfaces_dir            = '/etc/sysconfig/network-scripts'
@@ -29,6 +30,7 @@ class l23network::params {
       $ovs_datapath_package_name = 'kmod-openvswitch'
       $ovs_common_package_name   = 'openvswitch'
       $ovs_kern_module_name      = 'openvswitch'
+      $extra_tools               = 'iputils'
     }
     /(?i)darwin/: {
       $interfaces_dir            = '/tmp/1'
